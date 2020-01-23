cf3268cf4fb201002a8cec03d99b515338355c32
LilySu <lilyxsu@gmail.com>
2020-01-23 16:39:23 -0500

    added updated long phrase viztype 0

--- tallylib/sql.py
+++ tallylib/sql.py
@@ -1,37 +1,28 @@
 # tallylib/sql.py
 from datetime import datetime
-from datetime import timedelta
 from django.db import connection
 # Local
-# from yelp.models import AllReview # Django data model
+from yelp.models import Review
 '''
-2020-01-10 Pay attention to the indexes created on the tables.
+2020-01-10 Database table "tallyds.yelp_review" has the following index created.
+    "CREATE INDEX idx_review ON tallyds.yelp_review (business_id, datetime DESC);"
+2020-01-15 Database table "tallyds.job_logs has the following index created.
+    "
 '''
 
-###############################################################
-# tallyds.yelp_review
-###############################################################
+
 # Query with Django data models
 def getReviews(business_id, 
                starting_date, 
                ending_date):
     sql = f'''
-    SELECT date, 
-           text 
-    FROM tallyds.yelp_review
+    SELECT uuid, date, text FROM tallyds.yelp_review
     WHERE business_id = '{business_id}'
     AND datetime >= '{starting_date}'
     AND datetime <= '{ending_date}'
     ORDER BY datetime DESC;
     '''
-    # return [[record.date, record.text] 
-    #     for record in AllReview.objects.raw(sql)] # query by Django data model
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            return cursor.fetchall()
-    except Exception as e:
-        print(e)
+    return [[record.date, record.text] for record in Review.objects.raw(sql)]
 
 
 # Query without Django data models
@@ -47,13 +38,10 @@ def getReviewCountMonthly(business_id,
     ORDER BY 1 DESC, 2 DESC
     LIMIT {number_of_months};
     '''
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            # return a tuple
-            return cursor.fetchall()
-    except Exception as e:
-        print(e)
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        # return a tuple
+        return cursor.fetchall()
 
     
 def getLatestReviewDate(business_id):
@@ -64,14 +52,10 @@ def getLatestReviewDate(business_id):
     ORDER BY datetime DESC
     LIMIT 1;
     '''
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            # return [(datetime.datetime(2018, 10, 14, 0, 0),)]
-            return cursor.fetchall()
-    except Exception as e:
-        print(e)
-        return []
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        # return a datetime.datatime object
+        return cursor.fetchone()[0]
 
 
 def getLatestReviews(business_id, 
@@ -90,98 +74,22 @@ def getLatestReviews(business_id,
         # return a list of tuples
         return cursor.fetchall()
 
-
-def updateYelpReviews(business_id, data):
-    # data columns: datetime, star, text, review_id, user_id
-    if len(data)==0:
-        return 0 # returncode success
-
-    s0 = "INSERT INTO tallyds.yelp_review VALUES "
-    s1 = ""
-    for d in data:
-        d_datetime = d[0].strftime('%Y-%m-%d %H:%M:%S')
-        d_date = d[0].strftime('%Y-%m-%d')
-        d_time = d[0].strftime('%H:%M:%S')
-        d_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        d_text = d[2].replace("'", "''")
-        s1 = s1 + f"""\n    ('{d[3]}', \
-'{business_id}', \
-'{d[4]}', \
-{d[1]}, \
-'{d_datetime}', \
-'{d_date}', \
-'{d_time}', \
-'{d_text}', \
-'{d_now}'),"""
-
-#     s2 = '''
-# ON CONFLICT ON CONSTRAINT yelp_review_pkey
-# DO UPDATE SET
-#     business_id = excluded.business_id,
-#     user_id = excluded.user_id,
-#     stars = excluded.stars,
-#     datetime = excluded.datetime,
-#     date = excluded.date,
-#     time = excluded.time,
-#     text = excluded.text,
-#     timestamp = excluded.timestamp;    
-# '''
-    s2 = '''
-ON CONFLICT ON CONSTRAINT yelp_review_pkey
-DO NOTHING;  
-'''
-    sql= s0 +s1[:-1] + s2
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-    except Exception as e:
-        print(e)
-        return 1 # returncode 1 = failure
-    return 0 # returncode 0 = success
-
-
-###############################################################
-# tallyds.yelp_reivew_log
-###############################################################
-def getLatestYelpReviewLog(business_id):
+def getLatestYelpReviews(business_id, 
+                     limit=1):
     sql = f'''
-    SELECT datetime
-    FROM tallyds.yelp_review_log
+    SELECT date, 
+           text,
+           stars::INTEGER
+    FROM tallyds.yelp_review
     WHERE business_id = '{business_id}'
     ORDER BY datetime DESC
-    LIMIT 1;
+    LIMIT {limit};
     '''
     with connection.cursor() as cursor:
         cursor.execute(sql)
         # return a list of tuples
         return cursor.fetchall()
 
-
-def insertYelpReviewLog(business_id,
-                        date):
-    sql = f"""
-    INSERT INTO tallyds.yelp_review_log VALUES
-    (
-        '{business_id}',
-        '{date.strftime('%Y-%m-%d')}',
-        CURRENT_TIMESTAMP
-    )
-    ON CONFLICT ON CONSTRAINT yelp_review_log_pkey
-    DO UPDATE SET
-        datetime = excluded.datetime,
-        timestamp = excluded.timestamp
-    ;
-    """
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-    except Exception as e:
-        print(e)
-
-
-###############################################################
-# tallyds.job_log
-###############################################################
 def getJobLogs(business_id,
                limit=100):
     # uuid, 
@@ -222,154 +130,66 @@ def insertJobLogs(business_id,
         with connection.cursor() as cursor:
             cursor.execute(sql)
     except Exception as e:
+        print(sql)
         print(str(e))
         return 1 # returncode failure
     return 0 # returncode success
 
 
-###############################################################
-# tallyds.tally_business
-###############################################################
-def getTallyBusiness():
-    '''Businesses selected by Tally users'''
+def updateYelpReviews(business_id, data):
+    # data columns: datetime, star, text, review_id, user_id
+    if len(data)==0:
+        return 0 # returncode success
+
+    s1 = "INSERT INTO tallyds.yelp_review VALUES "
+    s2 = ""
+    for d in data:
+        d_datetime = d[0].strftime('%Y-%m-%d %H:%M:%S')
+        d_date = d[0].strftime('%Y-%m-%d')
+        d_time = d[0].strftime('%H:%M:%S')
+        d_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        d_text = d[2].replace("'", "''")
+        s2 = s2 + f"""\n    ('{d[3]}', \
+'{business_id}', \
+'{d[4]}', \
+{d[1]}, \
+'{d_datetime}', \
+'{d_date}', \
+'{d_time}', \
+'{d_text}', \
+'{d_now}'),"""
+
+#     s3 = '''
+# ON CONFLICT ON CONSTRAINT yelp_review_pkey
+# DO UPDATE SET
+#     business_id = excluded.business_id,
+#     user_id = excluded.user_id,
+#     stars = excluded.stars,
+#     datetime = excluded.datetime,
+#     date = excluded.date,
+#     time = excluded.time,
+#     text = excluded.text,
+#     timestamp = excluded.timestamp;    
+# '''
+    s3 = '''
+ON CONFLICT ON CONSTRAINT yelp_review_pkey
+DO NOTHING;  
+'''
+    sql= s1 +s2[:-1] + s3
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+    except:
+        return 1 # returncode 1 = failure
+    return 0 # returncode 0 = success
+
+
+def getTallyuserBusiness():
     sql = '''
-    SELECT business_id
-    FROM tallyds.tally_business;
+    SELECT DISTINCT business_id
+    FROM tallyds.tallyuser_business;
     '''
     with connection.cursor() as cursor:
         cursor.execute(sql)
         # return a list of strings
         return [r[0] for r in cursor.fetchall()]
-
-
-def isTallyBusiness(business_id):
-    sql = f"""
-    SELECT business_id
-    FROM tallyds.tally_business
-    WHERE business_id = '{business_id}';
-    """
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            # return a list of strings
-            return len(cursor.fetchall()) > 0
-    except Exception as e:
-        print(e)
-        return False
-
-
-def insertTallyBusiness(business_id):
-    sql = f"""
-    INSERT INTO tallyds.tally_business VALUES
-    (
-        '{business_id}',
-        CURRENT_TIMESTAMP
-    )
-    ON CONFLICT ON CONSTRAINT tally_business_pkey
-    DO NOTHING;
-    """
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            return 0 # success
-    except Exception as e:
-        print(e)
-        return 1 # error
-
-
-###############################################################
-# tallyds.ds_vizdata
-###############################################################
-def updateVizdata(business_id,
-                  viztype,
-                  vizdata):
-    sql=f'''\
-    INSERT INTO tallyds.ds_vizdata VALUES
-    (
-        '{business_id}',
-        {viztype},
-        current_timestamp,
-        '{vizdata.replace("'", "''")}'
-    )
-    ON CONFLICT ON CONSTRAINT ds_vizdata_pkey
-    DO UPDATE SET
-        timestamp = excluded.timestamp,
-        vizdata = excluded.vizdata
-    ;
-    '''
-    sql
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            return 0 # success
-    except Exception as e:
-        print(e)
-        return 1 # failure
-
-
-def checkVizdataTimestamp(business_id,
-                          viztype,
-                          days=14):
-    '''check whether vizdata has been generated within a period'''
-    timestamp = datetime.now() - timedelta(days=days)
-    sql=f'''
-    SELECT count(*)
-    FROM tallyds.ds_vizdata
-    WHERE business_id = '{business_id}'
-    AND viztype = {viztype}
-    AND timestamp >= '{timestamp.strftime('%Y-%m-%d')}'; 
-    '''
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql) 
-            return cursor.fetchall()[0][0]
-    except Exception as e:
-        print(e)
-        return 0
-
-
-def getLatestVizdata(business_id,
-                     viztype,
-                     days=14):
-    timestamp = datetime.now() - timedelta(days=days)
-    sql=f'''
-    SELECT vizdata, 
-           timestamp
-    FROM tallyds.ds_vizdata
-    WHERE business_id = '{business_id}'
-    AND viztype = {viztype}
-    AND timestamp >= '{timestamp.strftime('%Y-%m-%d')}'; 
-    '''
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql) 
-            # return a list of tuples
-            return cursor.fetchall()
-    except Exception as e:
-        print(e)   
-        return {}
-
-
-###############################################################
-# tallyds.ds_vizdata_log
-###############################################################
-def insertVizdataLog(business_id,
-                     viztype,
-                     triggeredby):
-    sql=f'''
-    INSERT INTO tallyds.ds_vizdata_log
-    VALUES (
-        uuid_generate_v4(),
-        '{business_id}',
-        {viztype},
-        '{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}',
-        {triggeredby}
-    );
-    '''
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql) 
-            return 0 # success
-    except Exception as e:
-        print(e)
-        return 1 # failure
--- yelp/views.py
+++ yelp/views.py
@@ -8,57 +8,22 @@ import json
 from tallylib.textrank import yelpTrendyPhrases
 from tallylib.scattertxt import getDataViztype0
 from tallylib.statistics import yelpReviewCountMonthly
-from tallylib.sql import getLatestVizdata
-from tallylib.sql import updateVizdata
-from tallylib.sql import insertVizdataLog
-from tallylib.sql import isTallyBusiness
-from tallylib.sql import insertTallyBusiness
-from jobs.tasks import task_yelpScraper
+from .models import YelpReview                # for data maintenance
+from .serializers import YelpReviewSerializer # for data maintenance
 
 
 # Query strings -> Main analytics
-# 2020-01-22 Here return codes and messages could be added 
-#     to make the APIs more user friendly.
 def home(request, business_id):
-    '''get data for views (APIs)'''
-    returncode, result = 0, ""
-    try:
-        if not isTallyBusiness(business_id):
-            task_yelpScraper([business_id], job_type=1) # triggered by end user
-            insertTallyBusiness(business_id)
-
-        viztype = request.GET.get('viztype')
-        viztype = int(viztype)
-        data = getLatestVizdata(business_id, viztype=viztype) # a list of tuples
-        if len(data) > 0:
-                result = data[0][0]
-                returncode = 0 # success
-        else:
-            if viztype == 0: # viztype0 and viztype3
-                result = json.dumps(getDataViztype0(business_id),
-                                    sort_keys=False)
-                returncode = 0 # success
-            elif viztype == 1:    
-                result = json.dumps(yelpTrendyPhrases(business_id), 
-                                    sort_keys=False)
-                returncode = 0 # success
-            elif viztype == 2:     
-                result = json.dumps(yelpReviewCountMonthly(business_id), 
-                                    sort_keys=False)
-                returncode = 0 # success
-            else: 
-                result = f"Error: There is no viztype {str(viztype)}."
-                returncode = 1 # error
-
-            # update table ds_vizdata and ds_vizdata_log
-            if returncode == 0:
-                updateVizdata(business_id, viztype, result)
-                insertVizdataLog(business_id, viztype, triggeredby=1) # triggered by end user
-    except Exception as e:
-        print(e)
-        result = f"Error: Wrong viztype {viztype}<br>{str(e)}"
-        returncode = 1 # error
-
+    viztype = request.GET.get('viztype')
+    if viztype == '1':
+        result = json.dumps(yelpTrendyPhrases(business_id), 
+                            sort_keys=False)
+    elif viztype == '2':
+        result = json.dumps(yelpReviewCountMonthly(business_id), 
+                            sort_keys=False)
+    else: # viztype0 and viztype3
+        result = json.dumps(getDataViztype0(business_id),
+                            sort_keys=False)
     return HttpResponse(result)
 
 
@@ -66,4 +31,22 @@ def home(request, business_id):
 def hello(request):
     result = "Hello, you are at the Tally Yelp Analytics home page."
     return HttpResponse(result)
+
+
+# example
+class YelpReviewCreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = YelpReview.objects.all()
+    serializer_class = YelpReviewSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+
+
+# example
+class YelpReviewDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+    queryset = YelpReview.objects.all()
+    serializer_class = YelpReviewSerializer
 
